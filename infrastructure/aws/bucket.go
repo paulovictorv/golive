cfc49f9568111f51ff54c66f839858c4e73b9d54
paulo_melo <paulo@goclip.com.br>
2019-11-14 16:17:26 -0300

    adding website default configuration for bucket

--- infrastructure/aws/bucket.go
+++ infrastructure/aws/bucket.go
@@ -1,17 +1,22 @@
 package aws
 
 import (
+	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func createBucket(bucketName string) {
+func createBucket(bucketName string) (string, error) {
+	bucketNameReference := aws.String(bucketName)
+
 	_, bucketError := s3Client.CreateBucket(&s3.CreateBucketInput{
-		Bucket: aws.String(bucketName),
+		Bucket: bucketNameReference,
 	})
 
-	parseAwsError(bucketError)
+	if bucketError != nil {
+		return "", errors.New(parseAwsError(bucketError))
+	}
 
 	publicPolicy := fmt.Sprintf("{\"Version\":\"2012-10-17\",\"Statement\": [{\"Sid\": \"AddPerm\",\"Effect\": \"Allow\","+
 		"\"Principal\": \"*\","+
@@ -19,9 +24,24 @@ func createBucket(bucketName string) {
 		"\"Resource\": \"arn:aws:s3:::%s/*\"}]}", bucketName)
 
 	_, policyError := s3Client.PutBucketPolicy(&s3.PutBucketPolicyInput{
-		Bucket: aws.String(bucketName),
+		Bucket: bucketNameReference,
 		Policy: aws.String(publicPolicy),
 	})
 
-	parseAwsError(policyError)
+	if policyError != nil {
+		return "", errors.New(parseAwsError(policyError))
+	}
+
+	_, webSiteError := s3Client.PutBucketWebsite(&s3.PutBucketWebsiteInput{
+		Bucket: bucketNameReference,
+		WebsiteConfiguration: &s3.WebsiteConfiguration{
+			IndexDocument: &s3.IndexDocument{Suffix: aws.String("index.html")},
+		},
+	})
+
+	if webSiteError != nil {
+		return "", errors.New(parseAwsError(webSiteError))
+	}
+
+	return fmt.Sprintf("%s.s3-website-%s.amazonaws.com", bucketName, *sess.Config.Region), nil
 }
